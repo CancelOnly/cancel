dc9188a21109c9f4ff597508f95a93306ed8c029
CancelOnly <70231421+CancelOnly@users.noreply.github.com>
2022-11-18 20:46:43 -0300

    added logo hover fx fixed color themes added hl theme minor changes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const darkButton = document.getElementById('dark');
 const lightButton = document.getElementById('light');
+const hlButton = document.getElementById('half-life');
 const body = document.body;
 
 // Cached Theme on reload
@@ -16,10 +17,20 @@ if (theme) {
 
 darkButton.onclick = () => {
     body.classList.replace('light', 'dark');
+    body.classList.replace('half-life', 'dark');
     localStorage.setItem('theme', 'dark');
 };
 
 lightButton.onclick = () => {
     body.classList.replace('dark', 'light');
+    body.classList.replace('half-life', 'light');
     localStorage.setItem('theme', 'light');
 };
+
+hlButton.onclick = () => {
+    body.classList.replace('dark','half-life')
+    body.classList.replace('light', 'half-life')
+    localStorage.setItem('theme', 'half-life')
+}
+
+/* hover fx */
