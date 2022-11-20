949559aea80b158139cd2bd581f6cafea029538f
CancelOnly <70231421+CancelOnly@users.noreply.github.com>
2022-11-20 19:25:01 -0300

    added scroll animation minor fixes

--- app.js
+++ app.js
@@ -80,3 +80,20 @@ const handleOnMouseMove = e => {
 for (const card of document.querySelectorAll(".card")) {
     card.onmousemove = e => handleOnMouseMove(e);
 }
+
+// Scroll Animation
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
